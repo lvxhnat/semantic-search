5cd78d58a35c3f7c2cbac7af9ca10faeb80adc8b
Loh Yi Kuang <yikuang5@gmail.com>
2024-07-15 22:21:57 +0800

    fix

--- backend/src/backend/app/services/chromadb.py
+++ backend/src/backend/app/services/chromadb.py
@@ -1,4 +1,8 @@
+import os
 import chromadb
+from dotenv import load_dotenv
 
-chroma_client = chromadb.PersistentClient(path="./data/chromadb/")
+load_dotenv()
+
+chroma_client = chromadb.PersistentClient(path=os.environ["CHROMADB_PATH"])
 msr_ = chroma_client.get_collection(name="msr2013-query")
