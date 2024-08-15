c713ac04cda1de637c506cf7ae5517cae539338f
Loh Yi Kuang <yikuang5@gmail.com>
2024-08-15 17:32:35 +0200

    models

--- backend/src/backend/app/services/model.py
+++ backend/src/backend/app/services/model.py
@@ -7,16 +7,19 @@ model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-128k-instruct",
     device_map="cuda",
     torch_dtype=torch.float16, # Reduce memory usage
-    trust_remote_code=True,
+    trust_remote_code=True, 
+    low_cpu_mem_usage=True
 )
+model.gradient_checkpointing_enable()
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct", torch_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct", torch_dtype=torch.float16, model_max_length=256)
 
 pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-    )
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.float16,
+)
 
 # Load pre-trained model and tokenizer
 reranker_model = AutoModelForSequenceClassification.from_pretrained('BAAI/bge-reranker-v2-m3')
