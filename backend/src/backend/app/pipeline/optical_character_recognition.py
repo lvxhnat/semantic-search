b52f0479a23aa26a110c23f019f86c81814480d2
Loh Yi Kuang <yikuang5@gmail.com>
2024-10-30 19:12:36 +0800

    push ocr model out to default load

--- backend/src/backend/app/pipeline/optical_character_recognition.py
+++ backend/src/backend/app/pipeline/optical_character_recognition.py
@@ -2,28 +2,13 @@ import re
 import tempfile
 from pathlib import Path
 from pdf2image import convert_from_path
-from transformers import AutoModel, AutoTokenizer, QuantoConfig
+from backend.app.services.model import ocr_model, ocr_tokenizer
 
 def image_to_text(file_path: str):
     txt_file_path = file_path.replace("pdf", "txt")
     if Path(txt_file_path).is_file():
         with open(txt_file_path, "r") as f:
             return f.read()
-    
-    ocr_model_id = 'ucaslcl/GOT-OCR2_0'
-
-    tokenizer = AutoTokenizer.from_pretrained(ocr_model_id, trust_remote_code=True)
-
-    model = AutoModel.from_pretrained(
-        ocr_model_id, 
-        trust_remote_code=True, 
-        low_cpu_mem_usage=True, 
-        device_map='cuda', 
-        use_safetensors=True, 
-        pad_token_id=tokenizer.eos_token_id,
-        quantization_config=QuantoConfig(weights="int8"),
-    )
-    model = model.eval().cuda()
 
     images = convert_from_path(file_path)
 
@@ -33,7 +18,7 @@ def image_to_text(file_path: str):
             # Save your image to this temporary file
             temp_file_path = temp_file.name
             image.save(temp_file_path)
-            res = model.chat(tokenizer, temp_file_path, ocr_type='format')
+            res = ocr_model.chat(ocr_tokenizer, temp_file_path, ocr_type='format')
             pdf_data.append(res)
     
     document_content = "\n".join(pdf_data)
--- backend/src/backend/app/services/model.py
+++ backend/src/backend/app/services/model.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer,AutoModelForSequenceClassification,  pipeline, QuantoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer,AutoModelForSequenceClassification,  pipeline, QuantoConfig, AutoModel
 
 torch.random.manual_seed(0)
 
@@ -28,3 +28,19 @@ pipe = pipeline(
 # Load pre-trained model and tokenizer
 reranker_model = AutoModelForSequenceClassification.from_pretrained('BAAI/bge-reranker-v2-m3')
 reranker_tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-reranker-v2-m3')
+
+
+ocr_model_id = 'ucaslcl/GOT-OCR2_0'
+
+ocr_tokenizer = AutoTokenizer.from_pretrained(ocr_model_id, trust_remote_code=True)
+
+ocr_model = AutoModel.from_pretrained(
+    ocr_model_id, 
+    trust_remote_code=True, 
+    low_cpu_mem_usage=True, 
+    device_map='cuda', 
+    use_safetensors=True, 
+    pad_token_id=tokenizer.eos_token_id,
+    quantization_config=QuantoConfig(weights="int8"),
+)
+ocr_model = model.eval().cuda()
