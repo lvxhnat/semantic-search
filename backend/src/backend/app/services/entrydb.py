5e3cf05c1fd0a2222b361058a7410a8174a0d5dd
Loh Yi Kuang <yikuang5@gmail.com>
2024-11-07 00:30:18 +0800

    fix: possible missing pandas file

--- backend/src/backend/app/services/entrydb.py
+++ backend/src/backend/app/services/entrydb.py
@@ -10,18 +10,17 @@ env_loaded = load_dotenv()
 root_path: str = Path(__file__).parents[4]
 
 SOURCE = "csv"
-SOURCE_PATH='https://drive.google.com/uc?id=' + os.environ["SAMPLE_DATA"].split('/')[-2]
+original_path = "https://drive.google.com/file/d/1cUODRGGLOXVdgy6RNLvOaRboEsr40cyb/view?usp=drive_link"
+SOURCE_PATH='https://drive.google.com/uc?id=' + original_path.split('/')[-2]
 
 if SOURCE is None or SOURCE_PATH is None: 
     raise ValueError(f"Source or Source Path should not be none. Please specify.")
 
 df = pd.DataFrame()
 if SOURCE == "csv":
-    df = (
-        pd.read_csv(SOURCE_PATH)
-        .dropna(subset = ['body'])
-        .assign(cleaned_body = lambda d: d["body"].apply(clean_text))
-    )
+    df = pd.read_csv(SOURCE_PATH)
+    print(f"Found columns in dataframe: {str(df.columns)}")
+    df = df.dropna(subset = ['body']).assign(cleaned_body = lambda d: d["body"].apply(clean_text))
     print(f"CSV Loaded from path {SOURCE_PATH}")
 elif SOURCE == "postgres":
     engine = engine.create_engine(SOURCE_PATH, echo=False)
